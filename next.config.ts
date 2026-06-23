0d0a0012bf5169dcba8d54107e0e5afa3c71ac13
Sanket <contact.sanketdhuri@gmail.com>
2026-06-23 12:33:11 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  basePath: isProd ? "/website-v2.0" : "",
-  assetPrefix: isProd ? "/website-v2.0/" : "",
+  /* config options here */
 };
 
 export default nextConfig;
